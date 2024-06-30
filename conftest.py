7fde31839b3fd267c040bfebb00b66651cdceb1c
Dzhalal Atabaev <djaldoit@gmail.com>
2024-06-30 13:21:08 +0300

    hw_guru 0.2

--- conftest.py
+++ conftest.py
@@ -2,28 +2,6 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(params=["1920x1080"])
-def desktop_screen_resolution(request):
-    width, height = map(int, request.param.split("x"))
-    browser.config.window_width = width
-    browser.config.window_height = height
-
-    yield
-
-    browser.quit()
-
-
-@pytest.fixture(params=["540x960"])
-def mobile_screen_resolution(request):
-    width, height = map(int, request.param.split("x"))
-    browser.config.window_width = width
-    browser.config.window_height = height
-
-    yield
-
-    browser.quit()
-
-
 @pytest.fixture(params=["1920x1080", "540x960"])
 def screen_resolution(request):
     width, height = map(int, request.param.split("x"))
